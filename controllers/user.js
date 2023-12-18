a14d00fa4c0aa2d3f5142e8468fe6d17d1cff7d0
Nikhil Sharma <nikhilsharma7908@gmail.com>
2023-12-18 08:37:14 +0530

    CRUD Apis

---
+++ controllers/user.js
@@ -0,0 +1,52 @@
+const { User } = require("../models/user");
+
+async function handleGetAllUsers(req, res) {
+  const allDbUsers = await User.find({});
+  return res.json(allDbUsers);
+}
+
+async function handleGetUserById(req, res) {
+  const user = await User.findById(req.params.id);
+  if (!user) return res.status(404).json({ error: "User not found" });
+  return res.json(user);
+}
+
+async function handleUpdateUserById(req, req) {
+  await User.findByIdAndUpdate(req.params.id, { lastName: "Changed" });
+  return res.json({ status: "Success" });
+}
+
+async function handleDeleteUer(req, res) {
+  await User.findByIdAndDelete(req.params.id);
+  return res.json({ status: "Success" });
+}
+
+async function handleCreateUser(req, res) {
+  const body = req.body;
+  if (
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title
+  ) {
+    return res.status(400).json({ msg: "All fields are req..." });
+  }
+
+  const result = await User.create({
+    firstName: body.first_name,
+    lastName: body.last_name,
+    email: body.email,
+    jobTitle: body.job_title,
+  });
+  return res.status(201).json({ msg: "success", id: result._id });
+}
+
+module.exports = {
+  handleGetAllUsers,
+  handleGetUserById,
+  handleUpdateUserById,
+  handleDeleteUer,
+  handleCreateUser,
+};
---
+++ redme.md
@@ -0,0 +1,18 @@
+REST API - JSON
+
+GET /users - HTML Document Render - Done
+GET/api/users - List all users JSON - Done
+
+
+GET /api/users/1 - Get the user with ID 1 - Done
+GET /api/users/2 - Get the user with ID 2 - Done
+
+Dynamic Path Parameters
+GET /api/users/:id
+:id -> variable | Dynamic
+
+POST /api/users - Create new user - Done
+
+PATCH /api/usesr/1 - Edit the user with ID 1 - Done
+
+DELETE /api/users/1 Delete the user with ID 1 - Done
